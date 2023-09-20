a40901a3849ec80271be332d52602e54179ccb3b
Pratik Panchal <pratikpanchal827@gmail.com>
2023-09-20 18:16:36 +0530

    @Input and @Output directive done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-app';
+
+  serverElements = [];
+
+  onServerAdded(serverData : {serverName:string, serverContent:string}){
+    this.serverElements.push({
+      type:'server',
+      name:serverData.serverName,
+      content:serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData : {serverName:string, serverContent:string}){
+    this.serverElements.push({
+      type:'blueprint',
+      name:blueprintData.serverName,
+      content:blueprintData.serverContent
+    });
+  }
 }
